ae048496864ccbaba89a747deeb3fe1d2b7c6cca
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-07-15 22:24:16 -0400

    fix: paths for images by adding vite.config.ts

--- library/src/main.ts
+++ library/src/main.ts
@@ -10,3 +10,5 @@ card.addEventListener('input', () => {
 name.addEventListener('input', () => {
   validateNameInput(name);
 });
+
+console.log(`Вёрстка валидная +10\nВёрстка семантическая +16\nВёрстка соответствует макету +54\nОбщие требования к верстке +20\n\nПоля input в форме принимают только буквы и "-" (Reader's name) или только цифры и "-" (Card number)`)
