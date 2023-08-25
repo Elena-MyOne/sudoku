78d29e4078da95e36e633ebd3ad4e86e1bfaf2ba
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-08-25 19:22:24 -0400

    feat: add carousel slider

--- library/src/main.ts
+++ library/src/main.ts
@@ -1,6 +1,11 @@
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
 import './style.scss';
 import { form, name, card, submitForm, validateCardInput, validateNameInput } from './ts/form';
 import { burger, list, toggleMenu, closeMenu } from './ts/burger';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
 burger?.addEventListener('click', toggleMenu);
 list?.addEventListener('click', toggleMenu);
@@ -16,4 +21,25 @@ name.addEventListener('input', () => {
   validateNameInput(name);
 });
 
-console.log(`Вёрстка соответствует макету. Ширина экрана 768px +26\n\nНи на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12\n\nВНа ширине экрана 768рх реализовано адаптивное меню +12`)
+const swiper = new Swiper('.slider', {
+  modules: [Navigation, Pagination],
+  spaceBetween: 25,
+  pagination: {
+    el: '.slider__pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1430: {
+      slidesPerView: 3,
+    },
+    1429: {
+      slidesPerView: 1,
+    },
+  },
+});
+
+// console.log(`Вёрстка соответствует макету. Ширина экрана 768px +26\n\nНи на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +12\n\nВНа ширине экрана 768рх реализовано адаптивное меню +12`);
--- library/src/ts/burger.ts
+++ library/src/ts/burger.ts
@@ -5,15 +5,14 @@ export const back = document.querySelector('.header__back') as HTMLElement;
 
 export const closeMenu = (e: MouseEvent) => {
   const target = e.target as HTMLElement;
-  console.log(target);
   if (list && back) {
-    if(target.closest('.header__link') || target === back) {
+    if (target.closest('.header__link') || target === back) {
       nav?.classList.remove('active');
       burger?.classList.remove('active');
       back?.classList.remove('active');
     }
   }
-}
+};
 
 export const toggleMenu = () => {
   if (nav && burger && back) {
@@ -21,4 +20,4 @@ export const toggleMenu = () => {
     burger.classList.toggle('active');
     back.classList.toggle('active');
   }
-}
+};
