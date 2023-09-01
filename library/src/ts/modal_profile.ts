26d3fc0450d8f7053b6052b58eb5f3c1adad10c7
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-09-01 16:17:34 -0400

    feat: render user name and avatar in profile model

--- library/src/ts/modal_profile.ts
+++ library/src/ts/modal_profile.ts
@@ -6,6 +6,8 @@ const buttonProfileLibraryCard = document.querySelector('.reader__button-profile
 const profileClose = document.querySelector('.profile__close img') as HTMLElement;
 const cardNumberProfile = document.querySelector('.card-number__number') as HTMLElement;
 export const cardNumberCopy = document.querySelector('.card-number__icon img') as HTMLElement;
+const profileName = document.querySelector('.profile__name') as HTMLElement;
+const profileAvatar = document.querySelector('.profile__avatar') as HTMLElement;
 
 export function handleProfileModal(e: MouseEvent) {
   const target = e.target as HTMLElement;
@@ -25,6 +27,8 @@ export function handleProfileUserInfo() {
 
   if (user) {
     cardNumberProfile.textContent = user.cardNumber;
+    profileName.textContent = `${user.name} ${user.lastName}`;
+    profileAvatar.textContent = `${user.name[0]}${user.lastName[0]}`.toUpperCase();
   }
 }
 
